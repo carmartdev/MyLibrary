537309534efe1e44c729def1e4704489d4824ac0
Vlad Rusakov <24524555+wervlad@users.noreply.github.com>
2021-02-05 19:31:50 +0400

    refactor system tests

--- features/bookstre.feature
+++ features/bookstre.feature
@@ -1,8 +1,7 @@
 Feature: Bookstore
-    Betty (user) would like to order some books online.
 
     Background: fill test database
-        Given a set of books
+        Given a set of books is available for sale
             | title         | author          | price |
             | Ohlolap       | Esuuz Phohoo    | 32.99 |
             | Aererey       | Pooepex Uojmeik | 14.99 |
@@ -10,22 +9,33 @@ Feature: Bookstore
             | Peele Zoos    | Teo Faquiey     | 88.99 |
             | Eupif Eengvoh | Ebohfee Acutoh  | 21.99 |
 
-    Scenario: Betty has heard about a cool new online bookstore.
-        When Betty opens bookstore homepage in her browser
+    Scenario: as buyer Betty would like to order some books online
+        Given Betty opens bookstore homepage in her browser
         Then she notices the page title and header mention 'Bookstore'
-            And she see book catalog on main page
+            And she can see book catalog on main page
 
-        When Betty clicks «add to cart» button near 1st book
-        Then she notices counter near cart button shows 1
+        Given shopping cart is empty
+        When Betty adds to cart book 'Ohlolap'
+        Then she notices counter near cart button showing '1'
+            And shopping cart contains book 'Ohlolap'
 
-        When Betty tries to add 1st book to cart again
-        Then she notices counter near cart button shows 1
+        Given shopping cart contains book 'Ohlolap'
+        When Betty tries to add book 'Ohlolap' again
+        Then she notices counter near cart button showing '1'
 
-        When Betty clicks «add to cart» button near 2nd book
-        Then she notices that counter value is now 2
+        Given shopping cart contains book 'Ohlolap'
+        When Betty adds to cart book 'Aererey'
+        Then she notices counter near cart button showing '2'
+            And shopping cart contains book 'Ohlolap'
+            And shopping cart contains book 'Aererey'
 
+        Given shopping cart contains book 'Ohlolap'
+            And shopping cart contains book 'Aererey'
         When Betty clicks «cart» button
-        Then she see 2 recently added books in her cart
+        Then she is redirected to cart page
+            And she can see book 'Ohlolap' in her cart
+            And she can see book 'Aererey' in her cart
 
+        Given Betty is on cart page
         When Betty clicks «checkout» button
-        Then she is redirected to the page with list of payment methods
+        Then she is redirected to checkout page
--- features/steps/bookstore.py
+++ features/steps/bookstore.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from time import sleep, time
 from selenium.common.exceptions import NoSuchElementException
-from store.models import Book
+from store.models import CartItem, Book
 
 def wait_for(fn, timeout=5):
     stime = time()
@@ -14,66 +14,77 @@ def wait_for(fn, timeout=5):
                 raise e
             sleep(0.5)
 
-@given("a set of books")
+@given("a set of books is available for sale")
 def fill_test_database(context):
     for b in context.table:
         Book(title=b["title"], author=b["author"], price=b["price"]).save()
 
-@when("Betty opens bookstore homepage in her browser")
+@given("Betty opens bookstore homepage in her browser")
 def load_home_page(context):
     context.browser.get(context.base_url)
 
 @then("she notices the page title and header mention '{title}'")
 def page_title_and_header_mention_bookstore(context, title):
     context.test.assertIn(title, context.browser.title)
+    context.test.assertIn(title,
+                          context.browser.find_element_by_tag_name('h1').text)
 
-@then("she see book catalog on main page")
-def she_see_book_catalog_on_main_page(context):
+@then("she can see book catalog on main page")
+def book_catalog_on_main_page(context):
     try:
         wait_for(lambda: context.browser.find_element_by_id("id_book_catalog"))
     except NoSuchElementException:
         context.test.fail("Book catalog not found on main page")
 
-@when("Betty clicks «add to cart» button near 1st book")
-def add_to_cart_1st_book(context):
-    table = wait_for(lambda: context.browser.find_element_by_id("id_book_catalog"))
-    rows = table.find_elements_by_tag_name("tr")
-    path = ".//button"
-    rows[0].find_element_by_xpath(".//button").click()
+@given("shopping cart is empty")
+def cart_is_empty(context):
+    context.test.assertEqual(CartItem.objects.count(), 0)
 
-@then("she notices counter near cart button shows 1")
-def counter_shows_one(context):
+@when("Betty adds to cart book '{title}'")
+def add_book_to_cart(context, title):
+    path = f"//td[contains(text(), '{title}')]/following::button"
+    wait_for(lambda: context.browser.find_element_by_xpath(path)).click()
+
+@then("she notices counter near cart button showing '{number:n}'")
+def counter_shows(context, number):
     cart = wait_for(lambda: context.browser.find_element_by_id("id_cart"))
-    context.test.assertIn("1", cart.text)
+    context.test.assertIn(str(number), cart.text)
 
-@when("Betty tries to add 1st book to cart again")
-def add_book_to_cart_repeatedly(context):
-    add_to_cart_1st_book(context)
+@step("shopping cart contains book '{title}'")
+def cart_contains_book(context, title):
+    try:
+        CartItem.objects.get(book=Book.objects.get(title=title))
+    except CartItem.DoesNotExist:
+        context.test.fail(f"Book {title} not found in shopping cart")
+    except Book.DoesNotExist:
+        context.test.fail(f"Book {title} does not exist in catalog")
 
-@when("Betty clicks «add to cart» button near 2nd book")
-def add_to_cart_2nd_book(context):
-    table = wait_for(lambda: context.browser.find_element_by_id("id_book_catalog"))
-    rows = table.find_elements_by_tag_name("tr")
-    path = ".//button"
-    rows[1].find_element_by_xpath(".//button").click()
-
-@then("she notices that counter value is now 2")
-def counter_shows_two(context):
-    cart = wait_for(lambda: context.browser.find_element_by_id("id_cart"))
-    context.test.assertIn("2", cart.text)
+@when("Betty tries to add book '{title}' again")
+def add_book_again(context, title):
+    context.execute_steps(f"When Betty adds to cart book '{title}'")
 
 @when("Betty clicks «cart» button")
 def click_cart_button(context):
-    raise NotImplementedError("STEP: When Betty clicks «cart» button")
+    wait_for(lambda: context.browser.find_element_by_id("id_cart")).click()
 
-@then("she see 2 recently added books in her cart")
-def two_books_in_the_cart(context):
-    raise NotImplementedError("STEP: Then she see 2 recently added books in her cart")
+@given("Betty is on cart page")
+@then("she is redirected to cart page")
+def buyer_can_see_her_cart(context):
+    context.test.assertIn("cart".upper(), context.browser.title.upper())
+
+@then("she can see book '{title}' in her cart")
+def buyer_can_see_book_in_her_cart(context, title):
+    try:
+        path = f"//td[contains(text(), '{title}')]"
+        wait_for(lambda: context.browser.find_element_by_xpath(path))
+    except NoSuchElementException:
+        context.test.fail(f"Buyer can't see book {title} in her cart")
 
 @when("Betty clicks «checkout» button")
 def click_checkout(context):
-    raise NotImplementedError("STEP: When Betty clicks «checkout» button")
+    wait_for(lambda: context.browser.find_element_by_id("id_checkout_button")
+             ).click()
 
-@then("she is redirected to the page with list of payment methods")
+@then("she is redirected to checkout page")
 def list_of_payment_methods_is_shown(context):
-    raise NotImplementedError("STEP: Then she is redirected to the page with list of payment methods")
+    context.test.assertIn("Checkout", context.browser.title)
