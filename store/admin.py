2125c16f8c80c80eb1241723c4abd4a8aef54ee1
Vlad Rusakov <24524555+wervlad@users.noreply.github.com>
2021-02-15 20:25:27 +0400

    add more fields to model

--- store/admin.py
+++ store/admin.py
@@ -3,6 +3,8 @@ from .models import Book
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("author", "title", "price")
+    list_display = ("title", "publisher", "publish_date", "description",
+                    "cover", "isbn_10", "isbn_13", "price",)
+    filter_horizontal = ("authors",)
 
 admin.site.register(Book, BookAdmin)
--- store/models.py
+++ store/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    key = models.CharField(max_length=10, primary_key=True)
+    name = models.TextField()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
+    key = models.CharField(max_length=11, primary_key=True)
     title = models.TextField()
-    author = models.TextField()
+    authors = models.ManyToManyField(Author)
+    publisher = models.TextField()
+    publish_date = models.CharField(max_length=50)
+    description = models.TextField()
+    cover = models.CharField(max_length=7)
+    isbn_10 = models.CharField(max_length=10)
+    isbn_13 = models.CharField(max_length=13)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    def get_absolute_url(self):
+        return reverse("store:book", args=[self.id])
